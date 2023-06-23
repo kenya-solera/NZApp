40f0390bf9aca8195c604769210e72f6a18bc400
anuevo <adrian.nuevo@solera.com>
2023-06-23 12:52:31 +0200

    First commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,17 @@
+import styles from './App.module.css'
+
+function App() {
+
+  return (
+
+    <>
+    
+      <main className={styles.App}>
+
+      </main>
+
+    </>
+  )
+}
+
+export default App
